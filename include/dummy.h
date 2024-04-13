33e088e24cf6e57a254336557b90229d518e88f5
Tyler <lorenarcdev@gmail.com>
2024-04-13 14:52:04 -0400

    Written docstrings for C++ source code.

--- include/dummy.h
+++ include/dummy.h
@@ -3,6 +3,9 @@
 #include <string>
 
 
+/* I don't know why, but I always resorted to using this kind of class
+ * when testing something. Stores basic infomation about a person. Meant
+ * to be exposed to python to be used as a base class.*/
 class Dummy {
   std::string name;
   std::string occupation;
@@ -20,10 +23,17 @@ public:
   int getAge();
   void setAge(int value);
 
+  /* By default, it would print the instance's properties to the console.
+   * It can be overridden in python, and that's thanks to the PyDummy
+   * helper class.*/
   virtual void doSomething();
 };
 
 
+/* According to the pybind11 documentation to make the member functions 
+ * like "doSomething" to be able to be overridden and modified in python,
+ * a helper class and trampoline must be defined. I'm guessing that a 
+ * trampoline is something like redirects virtual calls back to python.*/
 class PyDummy : public Dummy {
 public:
   using Dummy::Dummy;
--- include/python_script.h
+++ include/python_script.h
@@ -3,8 +3,14 @@
 #include <string>
 
 
+/* Stores the contents of a python script. Needs the file path to the 
+ * script on construction. This is class pretty much derives from string
+ * as I wanted to try something new, and see if it worked.*/
 class PythonScript : public std::string {
 public:
   PythonScript(std::string file_name);
+
+  /* For opening the script itself, and storing it's contents, formated 
+   * and executable. Is called on instance initialization.*/
   void loadScript(std::string file_name);
 };
